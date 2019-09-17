8ee53293d834160fc483b0509a83d784bb9c349d
upperlucky <upperLucky@gmail.com>
2019-09-17 20:57:52 +0800

    导航框架

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://www.devio.org/io/flutter_app/img/banner/100h10000000q7ght9352.jpg',
+    'https://dimg04.c-ctrip.com/images/300h0u000000j05rnD96B_C_500_280.jpg',
+    'http://pages.ctrip.com/hotel/201811/jdsc_640es_tab1.jpg',
+    'https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                autoplay: true,
+                itemCount: _imageUrls.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(_imageUrls[index], fit: BoxFit.fill);
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
